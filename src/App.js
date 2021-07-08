14d4da637ab49495f01d875ec68a1aa401222d3b
Meltem Ayar Akin <meltem.ayarakin@gmail.com>
2021-07-08 22:46:36 +1000

    changed back to learn react, tests are failing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch!
+          learn react!
         </a>
       </header>
     </div>
